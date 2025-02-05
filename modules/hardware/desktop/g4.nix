402bc52034360daa08b305952506d1fd223f5891
Maxwell Mattila <maxwellmattila@gmail.com>
2025-02-05 15:07:19 -0600

    readd kanata

--- modules/hardware/desktop/g4.nix
+++ modules/hardware/desktop/g4.nix
@@ -1,13 +1,35 @@
-{  pkgs, vars, host, ... }:
+{   config, lib, pkgs, host, ... }:
 
 with host;
 {
+  # Enable the uinput module
+  boot.kernelModules = [ "uinput" ];
+
+  # Enable uinput
+  hardware.uinput.enable = true;
+
+  # Set up udev rules for uinput
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  '';
+
+  # Ensure the uinput group exists
+  users.groups.uinput = { };
+
+  # Add the Kanata service user to necessary groups
+  systemd.services.kanata-internalKeyboard.serviceConfig = {
+    SupplementaryGroups = [
+      "input"
+      "uinput"
+    ];
+  };
+
   services = {
     ratbagd.enable = true;
     kanata =
       if hostName == "desktop" then {
         enable = true;
-        keyboards.g4 = {
+        keyboards.internalKeyboard = {
           config = ''
             (defsrc end)
             (deflayer default lalt)
